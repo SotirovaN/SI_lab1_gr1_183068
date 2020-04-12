84da4b7a163bc9ca064e749e37fca41019bb346b
SotirovaN <62435658+SotirovaN@users.noreply.github.com>
2020-04-12 20:08:19 +0200

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,71 @@
+import java.util.List;
+import java.util.ArrayList;
+class Student {
+	String index;
+	String firstName;
+	String lastName;
+	List<Integer>labPoints=new ArrayList<>();
+
+	//TODO constructor
+
+	public Student(String index, String firstName, String lastName, List<Integer> labPoints) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.labPoints = labPoints;
+	}
+
+	//TODO seters & getters
+
+
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public List<Integer> getLabPoints() {
+		return labPoints;
+	}
+
+	public void setLabPoints(List<Integer> labPoints) {
+		this.labPoints = labPoints;
+	}
+
+	public double getAverage() {
+		//TODO
+		double average = 0;
+		for(Integer points : labPoints)
+		{
+			average += points;
+		}
+		return average/labPoints.size();
+	}
+
+	public boolean hasSignature() {
+		//TODO
+		if(labPoints.size() > 8)
+		{
+			return true;
+		}
+		return false;
+	}
+}
